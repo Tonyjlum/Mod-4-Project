66a3670f9fff10e0f99e6363b9dd45034941d700
Chris Benedict <chrisjbenedict@gmail.com>
2019-02-07 11:15:02 -0500

    search form on splash page centered, want it left of center

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -4,33 +4,52 @@ const SearchForm = () => {
   return(
     <div class="translucent-form-overlay">
       <form>
-        <h3>Search for Properties</h3>
+        <h3>Find Your Toque</h3>
         <div class="row columns">
-          <label>Keyword
-            <input type="text" name="keyword" placeholder="Any" />
-          </label>
-        </div>
-        <div class="row columns">
-          <label>Property Status
+          <label>Location<br/>
             <select name="status" type="text">
-              <option>Any</option>
-              <option value="rent">Rent</option>
-              <option value="buy">Buy</option>
+              <option value="any">Any</option>
+              <option value="brooklyn">Brooklyn</option>
+              <option value="manhattan">Manhattan</option>
+              <option value="queens">Queens</option>
             </select>
           </label>
         </div>
         <div class="row columns">
-          <label>Property Type
+          <label>Type of Cuisines<br/>
             <select name="status" type="text">
-              <option>Any</option>
-              <option value="office">Office</option>
-              <option value="building">Building</option>
+              <option value="any">Any</option>
+              <option value="italian">Italian</option>
+              <option value="latin-american">Latin American</option>
+              <option value="indian">Indian</option>
+              <option value="american">American</option>
+              <option value="mediterranean">Mediterranean</option>
+              <option value="mexican">Mexican</option>
+              <option value="japanese">Japanese</option>
+              <option value="chinese">Chinese</option>
+              <option value="korean">Korean</option>
+              <option value="spanish">Spanish</option>
+              <option value="vegetarian-vegan">Vegetarian/Vegan</option>
+              <option value="vietnamese">Vietnamese</option>
+              <option value="other">Other</option>
             </select>
           </label>
         </div>
         <div class="row columns">
-          <label>Location
-            <input type="text" name="location" placeholder="Any" />
+          <label>Number of Guests<br/>
+            <select name="status" type="text">
+              <option value="one">1</option>
+              <option value="two">2</option>
+              <option value="three">3</option>
+              <option value="four">4</option>
+              <option value="five">5</option>
+              <option value="six">6</option>
+              <option value="seven">7</option>
+              <option value="eight">8</option>
+              <option value="9">9</option>
+              <option value="ten">10</option>
+              <option value="more-than-ten">more than 10</option>
+            </select>
           </label>
         </div>
         <div class="row">
