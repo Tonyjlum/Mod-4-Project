82df76855c1786068e8d4a48a0f50357354171b7
Chris Benedict <chrisjbenedict@gmail.com>
2019-02-07 10:23:48 -0500

    first push with foundation splash page styling

--- src/App.js
+++ src/App.js
@@ -7,18 +7,35 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+        <div class="hero-full-screen">
+
+          <div class="top-content-section">
+            <div class="top-bar">
+              <div class="top-bar-left">
+                <ul class="menu">
+                  <li class="menu-text"><img src="https://static.thenounproject.com/png/15320-200.png" alt="logo"/></li>
+                  <li><a href="#">toque</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+
+          <div class="middle-content-section">
+            <h1>Incredible Meals Brought to You</h1>
+            <button class="button large">Button</button>
+            <button class="button large">Button</button>
+          </div>
+
+          <div class="bottom-content-section" data-magellan data-threshold="0">
+            <a href="#main-content-section"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M24 12c0-6.627-5.373-12-12-12s-12 5.373-12 12 5.373 12 12 12 12-5.373 12-12zm-18.005-1.568l1.415-1.414 4.59 4.574 4.579-4.574 1.416 1.414-5.995 5.988-6.005-5.988z"/></svg></a>
+          </div>
+
+          </div>
+
+          <div id="main-content-section" data-magellan-target="main-content-section">
+          </div>
+
+
         </header>
       </div>
     );
