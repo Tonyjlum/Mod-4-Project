4faeb81de748bbc302edf278263bce8049874168
Chris Benedict <chrisjbenedict@gmail.com>
2019-02-13 19:40:20 -0500

    slight adjustments to about page

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,7 @@ const About = (props) => {
 
   return(
     <div className="about-page">
-      <h2>Chris Benedict</h2>
+      <h2>Chris J. Benedict</h2>
       <h2>&&</h2>
       <h2>Tony J. Lum</h2>
       <br/>
@@ -12,7 +12,7 @@ const About = (props) => {
       <h4>Flatiron School Teaching Staff</h4>
       <h4> 111918 Flatiron School Class</h4>
 
-      <img className="logo"width="70%" src={"https://upload.wikimedia.org/wikipedia/commons/6/61/FS_wiki.png"}/>
+      <img className="flatiron-logo" width="70%" src={"https://upload.wikimedia.org/wikipedia/commons/6/61/FS_wiki.png"}/>
     </div>
   )
 }
