32cab309fcb44d89a43754363a8a4caab0d606e1
Tonyjlum <tonyjlum@gmail.com>
2019-03-26 14:55:08 -0400

    fetch on load

--- src/index.js
+++ src/index.js
@@ -3,8 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import * as Const from '../const.js'
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+fetch(`${Const.ENDPOINT}`)
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
